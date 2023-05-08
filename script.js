c0512d6d2a92973ace3150a04be02f6150c4b0cf
Amelchenya <Sajgon20@gmail.com>
2023-05-08 22:20:40 +0300

    js

---
+++ script.js
@@ -0,0 +1,39 @@
+"use strict";
+
+alert("Привет, мир!");
+
+let planet;
+
+planet = "Sol3";
+
+alert(planet);
+
+let currentUserName;
+
+currentUserName = "Amelchenya";
+
+alert(currentUserName);
+
+let name = prompt("Ваше имя?", "");
+
+alert(name);
+
+let age = prompt('Сколько тебе лет?', 100);
+
+alert(`Тебе ${age} лет!`); // Тебе 100 лет!
+
+let isBoss = confirm("Ты здесь главный?");
+
+alert( isBoss );
+
+let userEyeColor = ('Серый')
+
+let user
+
+let userName
+
+userName = 'Вася'
+
+user = userName
+
+console.log (user)
