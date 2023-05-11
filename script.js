1d489318542a79f0c5a5c0a362d63bd9c71cc72f
Amelchenya <Sajgon20@gmail.com>
2023-05-11 11:45:01 +0300

    js

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 "use strict";
 
-for (let a = 0; a <= 100; a++)
-{
-    if (a % 3 !== 0, a % 5 !== 0)
-  {
-    console.log(a);
-  }
-    if (a % 3 === 0);
-  {
-    console.log('Fizz');
-  }
-    if (a % 5 === 0);
-  {
-    console.log('Buzz');
+for (let i = 1; i <= 100; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else {
+    console.log(i);
   }
 }
