0dd03722e874ffb2ddce48eb19db845e93ed605d
Amelchenya <Sajgon20@gmail.com>
2023-08-22 18:32:49 +0300

    hw3

--- HW3/HW3task8.js
+++ HW3/HW3task8.js
@@ -2,19 +2,19 @@ let str3 = 'ilya';
 
 
 
-let upperCase = function (a) {
-    let result = '';
-    for(let i = 0; i <= str3.length; i ++) {
-        result += a[i] * (i + 1);
+let upperCase = function (str) {
+    let result = [];
+    let newArr = Array.from(str);
+    for(let i = 0; i < newArr.length; i++) {
+        let repeated = newArr[i].repeat(i+1)
+        repeated.toUpperCase() + repeated.slice(1)
+        let toUP = repeated.charAt(0).toUpperCase() + repeated.slice(1) 
+        result.push(toUP)
+        newResult = result.join('--');
     }
-    return result
+    return newResult;
 }
 
-let splitted = str3.split('');
 
 
-
-let toUp = function(str3) {
-    return str3[0].toUpperCase() + str3.slice(1);
-}
-
+// let str10 = str3.repeat(3);
